3340df4bd90037c807c92738943146e5f59fe6ca
kenskodas <cybertesass@proton.me>
2023-07-25 00:10:44 -0700

    test

--- config/settings.py
+++ config/settings.py
@@ -117,12 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = '/static/'
-
-MEDIA_URL = '/media/'
-
   
-STATIC_ROOT = '/home/asancerime.com/public_html/penalti/MyApp/static/'
+STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]  
+STATIC_ROOT = BASE_DIR / "staticfiles" 
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"  # new
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
