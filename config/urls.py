94cb1107632118753ba5f7d69e54b8361cad9b73
kenskodas <cybertesass@proton.me>
2023-07-26 03:11:44 -0700

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import path, include, re_path
 from django.conf.urls.static import static
+from django.views.static import serve
 from django.conf import settings
 from django.conf.urls import handler404
+
 
 
 urlpatterns = [
